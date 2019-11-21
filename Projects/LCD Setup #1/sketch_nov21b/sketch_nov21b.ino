03d68c3d99ae9bd32207d7923d96851d8f4fc14e
AMeyburgh <annekin001@gmail.com>
2019-11-21 21:37:14 +0200

    First Day Setup

---
+++ Projects/LCD Setup #1/sketch_nov21b/sketch_nov21b.ino
@@ -0,0 +1,21 @@
+#include <LiquidCrystal.h>
+const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+}
+
+void loop() {
+  // Turn off the display:
+  lcd.noDisplay();
+  delay(500);
+  // Turn on the display:
+  lcd.display();
+  delay(500);
+}
